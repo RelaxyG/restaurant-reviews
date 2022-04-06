d3301700cf8b42ad124f6a76484833ee0ca91323
relaxyg <relaxyg4@gmail.com>
2022-04-06 15:32:06 +0600

    add reviews to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+  has_many :restaurants, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     member do
       get :chef
     end
+    resources :reviews, only: [ :new, :create ]
   end
+  resources :reviews, only: [ :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
